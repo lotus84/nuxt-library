19d891a02eeb49f072cf19de5e5b65b9100e362d
Elena Mikirtumova <mikirtumova84@gmail.com>
2023-08-25 22:48:35 +0400

    add i18n

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,10 +13,19 @@ module.exports = {
     "eslint:recommended",
     "@nuxtjs/eslint-config-typescript",
     "plugin:prettier/recommended",
+    "plugin:@intlify/vue-i18n/recommended",
   ],
   plugins: [],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "@intlify/vue-i18n/no-raw-text": [
+      process.env.NODE_ENV === "production" ? "error" : "warn",
+      {
+        ignoreText: [":", "?", "(", ")", ",", "/", "—", "+"],
+      },
+    ],
+    "@intlify/vue-i18n/no-missing-keys": "off", // not working with per component i18n option
+    "@intlify/vue-i18n/no-v-html": "off",
   },
 };
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="p-4">
-    <h1 class="text-gray-700 text-base">Hellow World!</h1>
+    <h1 class="text-gray-700 text-base">{{ $t("hello") }}</h1>
+    <form>
+      <label
+        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+        for="locale-select"
+        >{{ $t("language") }}:
+      </label>
+      <div class="relative">
+        <select
+          id="locale-select"
+          v-model="$i18n.locale"
+          class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+        >
+          <option
+            v-for="(locale, index) in $i18n.availableLocales"
+            :key="index"
+            :value="locale"
+          >
+            {{ locale }}
+          </option>
+        </select>
+      </div>
+    </form>
   </div>
 </template>
