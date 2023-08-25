f55f0e502631772a78b788cb9eff3ea0e4042e76
Elena Mikirtumova <mikirtumova84@gmail.com>
2023-08-25 16:11:52 +0400

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    screens: {
+      tablet: "768px",
+      laptop: "1024px",
+      desktop: "1280px",
+    },
+    extend: {
+      colors: {
+        linkedin: {
+          primary: "#0A66C2",
+          lighter: "#378fe9",
+          darker: "#004182",
+        },
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [],
+};
