2ef1bb991a9105a265808dd0694b9082a207c945
Elena Mikirtumova <mikirtumova84@gmail.com>
2023-08-27 20:39:02 +0400

    add header page

---
+++ components/TheHeader.vue
@@ -0,0 +1,20 @@
+<script setup>
+import ContentContainer from "~/components/ContentContainer.vue";
+import TheLogo from "~/components/TheLogo.vue";
+import LanguageToggle from "~/components/LanguageToggle.vue";
+</script>
+
+<template>
+  <header class="flex w-full border-b border-melrose border-solid">
+    <ContentContainer>
+      <div class="flex items-center justify-between w-full">
+        <div>
+          <TheLogo />
+        </div>
+        <div>
+          <LanguageToggle />
+        </div>
+      </div>
+    </ContentContainer>
+  </header>
+</template>
