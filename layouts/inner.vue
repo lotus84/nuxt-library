2321330409afdc5e78a19895d09dc1938bae6ea7
Elena Mikirtumova <mikirtumova84@gmail.com>
2023-08-27 20:37:11 +0400

    add layouts

---
+++ layouts/inner.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p>{{ $t('innerLayout') }}</p>
+    <slot />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <script setup>
+import TheIntro from "~/components/the-intro/TheIntro.vue";
 import BaseInput from "~/components/base/BaseInput.vue";
 </script>
 
 <template>
-  <div class="p-4">
-    <h1 class="text-gray-700 text-base">{{ $t("library") }}</h1>
+  <div>
+    <TheIntro />
     <div>
       <BaseInput
         :placeholder="$t('placeholder.email')"
@@ -12,27 +13,5 @@ import BaseInput from "~/components/base/BaseInput.vue";
         type="email"
       />
     </div>
-    <form>
-      <label
-        class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-        for="locale-select"
-        >{{ $t("language") }}:
-      </label>
-      <div class="relative">
-        <select
-          id="locale-select"
-          v-model="$i18n.locale"
-          class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-        >
-          <option
-            v-for="(locale, index) in $i18n.availableLocales"
-            :key="index"
-            :value="locale"
-          >
-            {{ locale }}
-          </option>
-        </select>
-      </div>
-    </form>
   </div>
 </template>
