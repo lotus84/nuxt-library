6575ded8b7c23eb73fc09c8003f9d7052366a18e
Elena <44998518+lotus84@users.noreply.github.com>
2023-08-29 08:26:27 +0400

    add sidebar (#5)

--- layouts/inner.vue
+++ layouts/inner.vue
@@ -1,6 +1,16 @@
+<script setup>
+import TheSidebar from "~/components/TheSidebar.vue";
+import TheHeader from "~/components/TheHeader.vue";
+</script>
 <template>
-  <div>
-    <p>{{ $t("innerLayout") }}</p>
-    <slot />
+  <div class="flex min-h-screen w-full bg-grey">
+    <TheSidebar />
+    <div class="flex flex-col grow justify-start items-start min-h-full">
+      <TheHeader />
+      <div class="flex flex-col justify-start items-start bg-grey p-5">
+        <p>{{ $t("innerLayout") }}</p>
+        <slot />
+      </div>
+    </div>
   </div>
 </template>
---
+++ pages/loans.vue
@@ -0,0 +1,11 @@
+<script setup>
+definePageMeta({
+  layout: "inner",
+});
+</script>
+
+<template>
+  <div class="flex flex-col grow w-full min-h-full">
+    {{ $t("loans") }}
+  </div>
+</template>
